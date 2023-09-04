fe45954a316b2b9f277163f280182ce7d7460adf
Bruno Hoinacki <brunohoinackib@gmail.com>
2023-09-04 19:08:20 -0300

    finished per hour

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
   styleUrls: ['./card-pricing.component.css']
 })
-export class CardPricingComponent {
+export class CardPricingComponent implements OnInit{
 
+	@Input()
+	gameType:String="Digital PS4"
+	@Input()
+	gamePrice:String ="R$ 399,90"
+
+	constructor(){}
+	ngOnInit(): void {
+
+	}
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
+	@Input()
+	gameCover:String = ""
+	@Input()
+	gameLabel:String=""
+	@Input()
+	gameType:String="XPTO | PS4"
+	@Input()
+	gamePrice:String ="R$ 399,90"
+
+
+	constructor(){}
+
+	ngOnInit():void{
+
+	}
 }
